cd3bf60fce7a03149a244f0a4f46c94f44889cdd
Godzuche <godxuche@gmail.com>
2025-04-13 13:32:56 +0100

    reset start destination

--- app/src/main/java/com/godzuche/bluechat/BlueChatNavHost.kt
+++ app/src/main/java/com/godzuche/bluechat/BlueChatNavHost.kt
@@ -6,8 +6,8 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import com.godzuche.bluechat.chat.presentation.BluetoothViewModel
-import com.godzuche.bluechat.chat.presentation.chat.chatRoute
 import com.godzuche.bluechat.chat.presentation.chat.chatScreen
+import com.godzuche.bluechat.chat.presentation.device_list.devicesRoute
 import com.godzuche.bluechat.chat.presentation.device_list.devicesScreen
 
 @Composable
@@ -15,7 +15,7 @@ fun BlueChatNavHost(
     bluetoothViewModel: BluetoothViewModel = hiltViewModel(),
     navController: NavHostController,
     modifier: Modifier = Modifier,
-    startDestination: String = chatRoute,
+    startDestination: String = devicesRoute,
 ) {
     NavHost(
         navController = navController,
